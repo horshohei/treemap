2cc7c2d20d56f9c78d998047191bd8189c05a854
farce <farce1524@gmail.com>
2024-06-05 16:40:46 +0900

    あ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,21 @@ import React from 'react';
 import TreeMap from '@/components/TreeMap';
 
 const data = [
-  { title: "埼玉", volume: 20, contents: ["大宮", "浦和", "与野"], class: "kanto" },
-  { title: "東京", volume: 40, contents: ["千代田", "台東"], class: "kanto" },
-  { title: "北海道", volume: 10, contents: ["札幌","厚岸"], class: "hokkaido" },
-  { title: "大阪", volume: 30, contents: ["羽曳野","岸和田"], class: "kinki" },
-  { title: "福岡", volume: 50, contents: ["博多", "大牟田"], class: "kyushu" },
-    { title: "愛知", volume: 30, contents: ["名古屋", "豊橋"], class: "chubu" },
-    { title: "広島", volume: 20, contents: ["広島", "福山"], class: "chugoku" },
-    { title: "香川", volume: 10, contents: ["高松", "丸亀"], class: "shikoku" },
-    { title: "岩手", volume: 10, contents: ["盛岡", "奥州"], class: "tohoku" },
+  { title: "埼玉", volume: 24, contents: ["大宮", "浦和", "与野"], class: "kanto" },
+  { title: "東京", volume: 28, contents: ["千代田", "台東"], class: "kanto" },
+  { title: "北海道", volume: 23, contents: ["札幌","厚岸"], class: "hokkaido" },
+  { title: "大阪", volume: 12, contents: ["羽曳野","岸和田"], class: "kinki" },
+  { title: "福岡", volume: 43, contents: ["博多", "大牟田"], class: "kyushu" },
+    { title: "愛知", volume: 23, contents: ["名古屋", "豊橋"], class: "chubu" },
+    { title: "広島", volume: 32, contents: ["広島", "福山"], class: "chugoku" },
+    { title: "香川", volume: 44, contents: ["高松", "丸亀"], class: "shikoku" },
+    { title: "岩手", volume: 8, contents: ["盛岡", "奥州"], class: "tohoku" },
+  { title: "神奈川", volume: 20, contents: ["横浜", "川崎"], class: "kanto" },
+  {title: "栃木", volume: 12, contents: ["宇都宮", "小山"], class: "kanto"},
+  {title: "群馬", volume: 13, contents: ["前橋", "高崎"], class: "kanto"},
+  {title: "長野", volume: 14, contents: ["長野", "松本"], class: "chubu"},
+    {title: "山梨", volume: 11, contents: ["甲府", "中央"], class: "chubu"},
+
 ];
 
 const Home: React.FC = () => {
